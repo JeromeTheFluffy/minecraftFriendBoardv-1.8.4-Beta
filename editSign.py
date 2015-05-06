c96424b3aa064b534a67604a56b25532dd3d09dc
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-05-06 10:19:31 -0600

    Update editSign.py

--- editSign.py
+++ editSign.py
@@ -1,2 +1,12 @@
-import mcpi.Block as block
-import mcpi.Minecraft as minecraft
+# this is beta software, covered by the GNU GPL v 2.0
+# this software is distributed without warranty and
+# is for testing only. any bug reports are accepted at
+# r3xth33nc0d3r@gmail.com. I appreciate the feedback
+
+from mcpi import Block as block
+from mcpi import Minecraft as minecraft
+# init connection to minecraft
+mc = minecraft.Minecraft.create()
+# create class 
+class writeToSign(Block):
+  
